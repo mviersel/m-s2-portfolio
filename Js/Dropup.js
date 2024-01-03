062314f4fdf50ff5963e5e5945ec45c580a37194
Martijn <119597405+mviersel@users.noreply.github.com>
2024-01-03 15:19:45 +0100

    Uploaden project

---
+++ Js/Dropup.js
@@ -0,0 +1,12 @@
+function dropup() {
+    const dropup = document.getElementById("dropup-balk");
+    dropup.classList.toggle("show");
+}
+
+
+// const dropup = document.getElementById("dropup-balk");
+
+// dropup.addEventListener("click", function() {
+//     dropup.classList.toggle("show");
+//     console.log("done")
+//})
